ae8857b28d4dea11f71056e8b5340603f0bd024b
renyoi <77758925+renyoi@users.noreply.github.com>
2021-02-19 19:03:33 -0500

    mostly complete game

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+onready var HUD = get_node("/root/Game/HUD")
 export var speed = 7
 
 
@@ -9,7 +10,7 @@ func _physics_process(delta):
 		$Laser.fire(Vector2(global_position.x, 0))
 	elif not Input.is_action_pressed("shoot") and $Laser.is_casting:
 		$Laser.stop()
-	
+   
 
 
 func get_input():
@@ -22,4 +23,5 @@ func get_input():
 
 
 func _on_Damage_body_entered(body):
+	HUD.update_health(-body.damage)
 	body.die()
